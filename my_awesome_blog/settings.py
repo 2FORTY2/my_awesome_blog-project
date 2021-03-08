bbe71dd90a47d7973a18875326d9d71240a1cf9c
MacBook Air <macbookair@MacBook-Air-MacBook.local>
2021-03-08 16:55:25 +0700

    Add Post model and upload it on Admin's panel

--- my_awesome_blog/settings.py
+++ my_awesome_blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -126,4 +127,4 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-MEDIA_URL = '/awesome_media/'
+MEDIA_URL = '/media/'
